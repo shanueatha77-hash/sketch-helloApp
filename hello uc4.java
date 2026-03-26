f9bb64a7f763724ba90f75d10d4c09d540213641
shanueatha77-hash <shanueatha77@gmail.com>
2026-03-26 15:42:44 +0530

    Add files via upload

---
+++ hello uc4.java
@@ -0,0 +1,9 @@
+public class UC4 {
+    public static void main(String[] args) {
+            String name = "Parker";
+            if (args.length>0){
+                name = args[0];
+            }
+            System.out.println("Hello, " + name + "!");
+    }
+}
